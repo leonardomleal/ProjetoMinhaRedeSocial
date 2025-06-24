3d98afca8b2896b44cf7b3f4b36d7614f9cabf0e
Leonardo Moisés Leal <leonardole@abcbrasil.com.br>
2025-06-24 19:38:07 -0300

    Propriedade data no comentário.

--- MinhaRedeSocial.Domain/Models/Postagens/Comentario.cs
+++ MinhaRedeSocial.Domain/Models/Postagens/Comentario.cs
@@ -5,6 +5,7 @@ namespace MinhaRedeSocial.Domain.Models.Postagens;
 public class Comentario
 {
     public Guid Id { get; set; }
+    public DateTime Data {  get; set; }
     public string Texto { get; set; }
     public Guid PostagemId { get; set; }
     public Guid UsuarioId { get; set; }
